5ba128eb62f875f3652ea864fa4c08602ae43dac
Rob Graziano <robgraziano@earthlink.net>
2014-11-01 23:27:20 -0400

    separated form templates

--- repairs/form_views.py
+++ repairs/form_views.py
@@ -1,15 +1,26 @@
 
 from django.contrib.formtools.wizard.views import SessionWizardView
 from django.shortcuts import render_to_response
+from .forms import InfoForm, CustomerForm, MachineForm, AssessmentForm
+
+FORMS = [("info", InfoForm),
+         ("customer", CustomerForm),
+         ("machine", MachineForm),
+         ("assessment", AssessmentForm)]
+
+TEMPLATES = {"info": "infoform.html",
+             "customer": "customerform.html",
+             "machine": "machineform.html",
+             "assessment": "assessmentform.html"}
 
 class ServiceOrderWizard(SessionWizardView):
-    template_name = 'service_form.html'
+    def get_template_names(self):
+        return [TEMPLATES[self.steps.current]]
 
     def done(self, form_list, **kwargs):
         form_data = process_form_data(form_list)
-        print("form data", form_data)
         #probably don't need to pass form_data to template
-        return render_to_response('done.html', {'form_data':form_data})
+        return render_to_response('done.html')
     
 def process_form_data(form_list):
     form_data = [form.cleaned_data for form in form_list]
--- repairs/urls.py
+++ repairs/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from repairs.forms import InfoForm, CustomerForm, MachineForm, AssessmentForm
-from repairs.form_views import ServiceOrderWizard
+from repairs.form_views import ServiceOrderWizard, FORMS
 
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'repairs.views.repair_home', name='repair_home'),
     #url(r'repair-form/$', 'repairs.views.repair_form', name='repair_form'),
     url(r'individual-report/(?P<rma_number>\d+)/$', 'repairs.views.individual_report', name='individual_report'),
-    url(r'^repair-form/$', ServiceOrderWizard.as_view([InfoForm, CustomerForm, MachineForm, AssessmentForm])),
+    url(r'^repair-form/$', ServiceOrderWizard.as_view(FORMS)),
 )
