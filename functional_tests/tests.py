722fc6193caa1c0165dccaa38409f1ad76b95c1e
Rob Graziano <robgraziano@earthlink.net>
2015-11-09 15:12:51 -0500

    test add engineer

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,5 +1,7 @@
 from django.test import LiveServerTestCase
 from unittest import skip
+import service_orders.tests.data as data
+from service_orders.models import Machine, ServiceEngineer, Address, Customer, Part, ServiceLog
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -12,7 +14,6 @@ class SOTestCase(LiveServerTestCase):
 	def tearDown(self):
 		self.browser.quit()
 
-	@skip("")
 	def test_we_can_reach_front_page_and_login(self):
 		""" Test that front page is working and we can log in successfully """
 		self.browser.get('%s%s' % (self.live_server_url, '/'))
@@ -38,34 +39,26 @@ class SOTestCase(LiveServerTestCase):
 		self.browser.implicitly_wait(3)
 
 	def test_add_engineer(self):
-		""" Test that we can navigate from the front page and successfully add an engineer """
+		""" Test that we can successfully add an engineer """
 
-		# Maybe change this to navigation from home instead of using direct url?
-		# ===================================================================================
-		self.browser.get('%s%s' % (self.live_server_url, '/'))
-		username_field = self.browser.find_element_by_name('username')
-		username_field.send_keys('rob')
+		self.browser.get('%s%s' % (self.live_server_url, '/serviceorders/engineerform/'))
 
-		password_field = self.browser.find_element_by_name('password')
-		password_field.send_keys('CoffeeHouse')
-		password_field.send_keys(Keys.RETURN)
+		dropdown = self.browser.find_element_by_class_name("dropdown-toggle").click()
+		engineer_button = self.browser.find_element_by_link_text('Engineer').click()
 
-		self.browser.get('%s%s' % (self.live_server_url, '/accounts/loggedin/'))
-		continue_button = self.browser.find_element_by_tag_name('input').click()
-		if continue_button:
+		first_name_field = self.browser.find_element_by_id('id_first_name')
+		first_name = "asdfasdfasdf"
+		first_name_field.send_keys(first_name)
+		last_name_field = self.browser.find_element_by_id('id_last_name')
+		last_name = "badarbvaer"
+		last_name_field.send_keys(last_name)
+		last_name_field.send_keys(Keys.RETURN)
 
-			print("We made it!")
-			"""
-			dropdown = self.browser.find_element_by_class_name("dropdown-toggle").click()
-			engineer_button = self.browser.find_element_by_link_name('Engineer').click()
 
-			first_name_field = self.browser.find_element_by_id('id_first_name')
-			first_name_field.send_keys('Joey')
-			last_name_field = self.browser.find_element_by_id('id_last_name')
-			last_name_field.send_keys('Smith')
-			last_name_field.send_keys(Keys.RETURN)
-			"""
-		print("We didn't make it.")
+		engineer = ServiceEngineer.objects.get(last_name=last_name)
+
+		self.assertEqual(engineer.first_name, first_name, "First Names didn't match!")
+		self.assertEqual(engineer.last_name, last_name, "Last names didn't match!")
 
 		self.browser.implicitly_wait(3)
 
